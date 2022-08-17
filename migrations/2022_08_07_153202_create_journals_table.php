5519cbe64f2d09c1e2d7f3ed4f925f8f8c430c63
nymlis <107400596+nymlis@users.noreply.github.com>
2022-08-17 21:55:37 +0900

    Add files via upload

---
+++ migrations/2022_08_07_153202_create_journals_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('journals', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->string('debit_accounts');
+            $table->unsignedInteger('debit_price');
+            $table->string('credit_accounts');
+            $table->unsignedInteger('credit_price');
+            $table->text('summary')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('journals');
+    }
+};
