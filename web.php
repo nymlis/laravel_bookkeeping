b02402a609610a3180df96934715f4003258b769
nymlis <107400596+nymlis@users.noreply.github.com>
2022-08-17 20:59:11 +0900

    Add files via upload

--- web.php
+++ web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JournalController;
+use App\Http\Controllers\AccountController;
 use App\Models\Journal;
 use Illuminate\Routing\Route as RoutingRoute;
 
@@ -17,7 +18,10 @@ use Illuminate\Routing\Route as RoutingRoute;
 */
 
 Route::get('/', [JournalController::class, 'index'])
-    ->name('journals.index');
+    ->name('index');
+
+Route::get('/journals', [JournalController::class, 'view_journals'])
+    ->name('journals');
 
 Route::post('/journals/create', [JournalController::class, 'create'])
     ->name('journals.create');
@@ -34,3 +38,36 @@ Route::delete('/journals/{journal}/destroy', [JournalController::class, 'destroy
     ->name(('journals.destroy'))
     ->where('journal', '[0-9]+');
 
+
+Route::get('/general_ledger',[AccountController::class, 'view_ledger'])
+    ->name('general_ledger');
+
+Route::post('/general_ledger/show',[JournalController::class, 'ledger_show'])
+    ->name('general_ledger.show');
+
+
+Route::get('/balance_sheet',[JournalController::class, 'BS_show'])
+    ->name('balance_sheet');
+
+
+Route::get('/income_statement',[JournalController::class, 'PL_show'])
+    ->name('income_statement');
+
+
+Route::get('/accounts', [AccountController::class, 'view_accounts'])
+    ->name('accounts');
+
+Route::post('/accounts/create', [AccountController::class, 'create'])
+    ->name('accounts.create');
+
+Route::get('/accounts/{account}/edit', [AccountController::class, 'edit'])
+    ->name('accounts.edit')
+    ->where('account', '[0-9]+');
+
+Route::patch('/accounts/{account}/update', [AccountController::class, 'update'])
+    ->name('accounts.update')
+    ->where('account', '[0-9]+');
+
+Route::delete('/accounts/{account}/destroy', [AccountController::class, 'destroy'])
+    ->name(('accounts.destroy'))
+    ->where('account', '[0-9]+');
